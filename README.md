9b10d1d10c668e4199e1735e84de9f797de02375
AL3ARABIY <alarabiymahdi1980@gmail.com>
2024-05-10 23:59:43 +0100

    define two users with username and password. (User and Admin)

---
+++ README.md
@@ -0,0 +1,2 @@
+## Spring boot with spring security and thymeleaf
+- Source: https://www.baeldung.com/spring-security-thymeleaf
--- src/main/java/org/data/redroleplay/configurations/SecurityConfig.java
+++ src/main/java/org/data/redroleplay/configurations/SecurityConfig.java
@@ -1,7 +1,9 @@
 package org.data.redroleplay.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +11,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
