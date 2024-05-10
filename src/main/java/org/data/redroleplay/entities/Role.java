c4631e8f2111d1876103fdbdec2bd767317d9bb5
AL3ARABIY <alarabiymahdi1980@gmail.com>
2024-05-11 00:30:26 +0100

    create user and role entities

---
+++ src/main/java/org/data/redroleplay/entities/Role.java
@@ -0,0 +1,21 @@
+package org.data.redroleplay.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
---
+++ src/main/java/org/data/redroleplay/entities/User.java
@@ -0,0 +1,35 @@
+package org.data.redroleplay.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collection;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+
+    private Collection<Role> roles;
+}
