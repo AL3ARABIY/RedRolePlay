cee3aaaa68b4e8dd67455e1908f51d959c6a4d16
AL3ARABIY <alarabiymahdi1980@gmail.com>
2024-05-10 23:08:54 +0100

    Generate Spring boot project (Java 21)

---
+++ src/main/java/org/data/redroleplay/RedRolePlayApplication.java
@@ -0,0 +1,13 @@
+package org.data.redroleplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedRolePlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedRolePlayApplication.class, args);
+    }
+
+}
