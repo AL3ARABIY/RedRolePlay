4aaf5c16929f0244a6cbbf50003aba8f555dd328
AL3ARABIY <alarabiymahdi1980@gmail.com>
2024-05-11 00:40:02 +0100

    create UserRegistrationDto

---
+++ src/main/java/org/data/redroleplay/dtos/UserRegistrationDto.java
@@ -0,0 +1,21 @@
+package org.data.redroleplay.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationDto {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private LocalDate birthDate;
+    private String email;
+    private String password;
+}
