f07ecfc7670f79aade65cb34918f0924181f0559
AL3ARABIY <alarabiymahdi1980@gmail.com>
2024-05-15 00:23:55 +0100

    create the base of whitelist page

--- src/main/java/org/data/redroleplay/repositories/website/WhitelistRequestRepository.java
+++ src/main/java/org/data/redroleplay/repositories/website/WhitelistRequestRepository.java
@@ -1,6 +1,7 @@
 package org.data.redroleplay.repositories.website;
 
 import org.data.redroleplay.entities.website.WhitelistRequest;
+import org.data.redroleplay.enums.WhitelistRequestStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WhitelistRequestRepository extends JpaRepository<WhitelistRequest, Long>{
     Page<WhitelistRequest> findAllByUserId(Long userId , Pageable pageable);
+
+    Page<WhitelistRequest> findAllByStatus(WhitelistRequestStatus status , Pageable pageable);
 }
