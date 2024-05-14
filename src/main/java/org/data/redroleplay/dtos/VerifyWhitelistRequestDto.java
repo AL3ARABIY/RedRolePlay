ea8962ab06189a3557c465647a88e8cd1d37fe04
AL3ARABIY <alarabiymahdi1980@gmail.com>
2024-05-14 12:02:04 +0100

    add field validation to WhileListRequest and VerifyWhiteListRequest

---
+++ src/main/java/org/data/redroleplay/dtos/VerifyWhitelistRequestDto.java
@@ -0,0 +1,32 @@
+package org.data.redroleplay.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.data.redroleplay.annotations.EnumValue;
+import org.data.redroleplay.enums.WhitelistRequestStatus;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerifyWhitelistRequestDto {
+
+    @NotNull(message = "Field.required")
+    private Long id;
+
+    @NotNull(message = "Field.required")
+    @EnumValue(enumClass = WhitelistRequestStatus.class, message = "NotValidEnum.whitelistRequest.status")
+    private String status;
+
+    @NotNull(message = "Field.required")
+    @Size(min = 10, max = 5000, message = "{Field.size}")
+    private String reason;
+
+    @Size(min = 0, max = 5000, message = "{Field.size}")
+    private String note;
+}
--- src/main/java/org/data/redroleplay/dtos/WhitelistRequestDto.java
+++ src/main/java/org/data/redroleplay/dtos/WhitelistRequestDto.java
@@ -1,11 +1,17 @@
 package org.data.redroleplay.dtos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.data.redroleplay.annotations.EnumValue;
 import org.data.redroleplay.enums.CharacterGender;
 import org.data.redroleplay.enums.CharacterOrientation;
+
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -13,18 +19,29 @@ import org.data.redroleplay.enums.CharacterOrientation;
 @NoArgsConstructor
 public class WhitelistRequestDto {
 
+    @NotNull(message = "Field.required")
     private String characterFirstName;
 
+    @NotNull(message = "Field.required")
     private String characterLastName;
 
-    private CharacterGender characterGender;
+    @NotNull(message = "Field.required")
+    @EnumValue(enumClass = CharacterGender.class, message="NotValidEnum.whitelistRequest.characterGender")
+    private String characterGender;
 
+    @NotNull(message = "Field.required")
     private String characterOrigin;
 
-    private String characterBirthDate;
+    @NotNull(message = "Field.required")
+    @Past(message = "BirthDate.past")
+    private LocalDate characterBirthDate;
 
-    private CharacterOrientation characterOrientation;
+    @NotNull(message = "Field.required")
+    @EnumValue(enumClass = CharacterOrientation.class, message="NotValidEnum.whitelistRequest.characterOrientation")
+    private String characterOrientation;
 
+    @NotNull(message = "Field.required")
+    @Size(min = 100, max = 5000, message = "{Field.size}")
     private String characterStory;
 
 }
