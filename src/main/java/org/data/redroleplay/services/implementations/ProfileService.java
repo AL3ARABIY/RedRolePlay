981cf55e00f9089032ec82e1c57a99f6f1dd1ca3
AL3ARABIY <alarabiymahdi1980@gmail.com>
2024-05-15 21:38:12 +0100

    change the scope of ProfileService to session

--- src/main/java/org/data/redroleplay/services/implementations/ProfileService.java
+++ src/main/java/org/data/redroleplay/services/implementations/ProfileService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Scope("prototype")
+@Scope("session")
 public class ProfileService {
 
     private final UserService userService;
